47ab3d9cde34180c6b15f29e033d954e91f62c74
abi-widiarta <abiwidiarta03@gmail.com>
2023-02-09 16:51:21 +0700

    Added Rainbow Color, Size,Color Indicator

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
+// ELEMENT SELECTION
 const body = document.body;
 const board = document.querySelector(".sketch-board");
 const btnClear = document.querySelector(".btn-clear");
 const wrapper = document.querySelector(".sketch-container");
 const colorPicker = document.querySelector("#color-input");
+const sizeIndicator = document.querySelector(".size-display");
+const colorModeIndicator = document.querySelector(".color-mode-display");
 
+// GLOBAL VAR
 let clicked = false;
 let color = colorPicker.value;
+let colorRainbow = false;
 let tilesCount = 10;
 
-const changeTilesCount = (size) => {
-  tilesCount = 10;
-  clearBoard();
-  return (tilesCount += size);
-};
-
+// SPINNABLE BUTTON
 const spinner = Draggable.create("#btn-rounded-size", {
   type: "rotation",
   inertia: true,
-  bounds: { minRotation: 0, maxRotation: -90 },
+  bounds: { minRotation: 0, maxRotation: 90 },
   onDrag: () => {
-    // console.log(Math.round((spinner[0].rotation / -90) * 48));
+    let sizeDisplay = Math.round((spinner[0].rotation / 90) * 54) + 10;
+    sizeIndicator.textContent = `${sizeDisplay} x ${sizeDisplay}`;
   },
   onRelease: () => {
-    changeTilesCount(Math.round((spinner[0].rotation / -90) * 20));
+    changeTilesCount(Math.round((spinner[0].rotation / 90) * 54));
     renderTilesCount();
   },
 });
@@ -31,12 +32,40 @@ const spinner2 = Draggable.create("#btn-rounded-color", {
   type: "rotation",
   inertia: true,
   bounds: { minRotation: 0, maxRotation: 90 },
-  onDrag: () => {
-    console.log(spinner2[0].rotation);
+  onDrag: () => {},
+  onRelease: () => {
+    changeColorMode(spinner2[0].rotation);
   },
 });
 
-// let tileWidth = board.clientWidth / 10;
+// FUNCTIONS
+const changeTilesCount = (size) => {
+  tilesCount = 10;
+  clearBoard();
+  return (tilesCount += size);
+};
+
+const getRandomColorNum = () => {
+  return Math.round(Math.random() * 255);
+};
+
+const getRandomRainbowColor = () => {
+  let red = getRandomColorNum();
+  let green = getRandomColorNum();
+  let blue = getRandomColorNum();
+
+  return `rgb(${red},${green},${blue})`;
+};
+
+const changeColorMode = (value) => {
+  if (value >= 45) {
+    colorRainbow = true;
+    colorModeIndicator.textContent = "Rainbow";
+  } else {
+    colorRainbow = false;
+    colorModeIndicator.textContent = "Solid";
+  }
+};
 
 const renderTilesCount = () => {
   board.style.gridTemplateColumns = `repeat(${tilesCount}, auto)`;
@@ -53,13 +82,18 @@ const renderTilesCount = () => {
       event.preventDefault();
     });
 
-    tileElement.addEventListener("mousedown", () => {
-      tileElement.style.backgroundColor = color;
-    });
-
-    tileElement.addEventListener("mousemove", () => {
+    tileElement.addEventListener("mouseenter", () => {
+      let randomNum;
       if (clicked) {
-        tileElement.style.backgroundColor = color;
+        if (colorRainbow) {
+          randomNum = getRandomColorNum();
+          console.log(randomNum);
+          tileElement.style.backgroundColor = getRandomRainbowColor();
+          console.log(colorRainbow, "yes");
+        } else {
+          tileElement.style.backgroundColor = color;
+          console.log(colorRainbow);
+        }
       }
     });
 
